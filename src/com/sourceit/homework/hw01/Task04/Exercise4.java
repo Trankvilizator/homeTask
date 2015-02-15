1e255e10fae8e586f8b2f9f16151e1c5a2287b41
Trankvilizator <ILutsan@gmail.com>
2015-02-15 20:52:10 +0200

    The sum of the digits of n

---
+++ src/com/sourceit/homework/hw01/Task04/Exercise4.java
@@ -0,0 +1,12 @@
+package com.sourceit.homework.hw01.Task04;
+
+/**
+ * Created by User on 15.02.2015.
+ */
+public class Exercise4 {
+    int n;
+
+    public static void main(String[] args) {
+        
+    }
+}
