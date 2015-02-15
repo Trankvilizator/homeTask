c959f85aa7e266e74a043d9a72fac378d694fe73
Trankvilizator <ILutsan@gmail.com>
2015-02-15 21:14:26 +0200

    The sum of the digits of n

--- src/com/sourceit/homework/hw01/Task04/Exercise4.java
+++ src/com/sourceit/homework/hw01/Task04/Exercise4.java
@@ -1,12 +1,23 @@
 package com.sourceit.homework.hw01.Task04;
 
+import java.util.Scanner;
+
 /**
  * Created by User on 15.02.2015.
  */
 public class Exercise4 {
     int n;
+    int s=0;
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int n = in.nextInt();
+        System.out.println((n=n/10));
+        System.out.println((n=n/10));
+        System.out.println("the sum of the digits of n=");
+
+
 
     }
 }
