d5ae176614fe7b7be66b7f36daf1db3c0b2f45ff
Trankvilizator <ILutsan@gmail.com>
2015-02-15 20:47:30 +0200

    Del this class

--- src/com/sourceit/homework/hw01/Math.java
+++
@@ -1,7 +0,0 @@
-package com.sourceit.homework.hw01;
-
-/**
- * Created by User on 15.02.2015.
- */
-public class Math {
-}
--- src/com/sourceit/homework/hw01/Test.java
+++
@@ -1,7 +0,0 @@
-package com.sourceit.homework.hw01;
-
-/**
- * Created by Source_it-1 on 03.02.2015.
- */
-public class Test {
-}
