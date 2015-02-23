2ccabf4ea4c79150e16bdb2fdab6276fdf55df25
Trankvilizator <ILutsan@gmail.com>
2015-02-23 22:06:54 +0200

    The sum of digits

---
+++ src/com/sourceit/homework/hw02/Task01/Sum.java
@@ -0,0 +1,26 @@
+package com.sourceit.homework.hw02.Task01;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
+/**
+ * Created by User on 23.02.2015.
+ */
+public class Sum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = in.nextInt();
+
+        int i=0;
+
+        do {
+            i++;
+            n=n/10;
+        }
+        while (n>0);
+        System.out.println("The sum of digits = "+i);
+    }
+}
